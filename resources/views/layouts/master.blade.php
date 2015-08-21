c48d8fb3e5cbbd4d628b77adb2085d1836c24b15
nutsdo <ldingtao@163.com>
2015-08-21 16:28:31 +0800

    添加用户登录功能

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lvdingtao
+ * Date: 8/21/15
+ * Time: 10:37 AM
+ */
+ ?>
+ <!DOCTYPE html>
+ <html lang="zh-CN">
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+ 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ 	<meta name="description" content="Xenon Boostrap Admin Panel" />
+ 	<meta name="author" content="" />
+
+ 	<title>Xenon - Login</title>
+
+ 	{!! Html::style("http://fonts.useso.com/css?family=Arimo:400,700,400italic") !!}
+ 	{!! Html::style("assets/css/fonts/linecons/css/linecons.css") !!}
+ 	{!! Html::style("assets/css/fonts/fontawesome/css/font-awesome.min.css") !!}
+ 	{!! Html::style("assets/css/bootstrap.css") !!}
+ 	{!! Html::style("assets/css/xenon-core.css") !!}
+ 	{!! Html::style("assets/css/xenon-forms.css") !!}
+ 	{!! Html::style("assets/css/xenon-components.css") !!}
+ 	{!! Html::style("assets/css/xenon-skins.css") !!}
+ 	{!! Html::style("assets/css/custom.css") !!}
+
+    {!! Html::script("assets/js/jquery-1.11.1.min.js") !!}
+
+ 	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+ 	<!--[if lt IE 9]>
+        {!! Html::script("https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js") !!}
+        {!! Html::script("https://oss.maxcdn.com/respond/1.4.2/respond.min.js") !!}
+ 	<![endif]-->
+
+
+ </head>
+ <body class="page-body login-page">
+
+
+ @yield('container')
+
+
+
+ <!-- Bottom Scripts -->
+ {!! Html::script("assets/js/bootstrap.min.js") !!}
+
+ {!! Html::script("assets/js/TweenMax.min.js") !!}
+ {!! Html::script("assets/js/resizeable.js") !!}
+ {!! Html::script("assets/js/joinable.js") !!}
+ {!! Html::script("assets/js/xenon-api.js") !!}
+ {!! Html::script("assets/js/xenon-toggles.js") !!}
+ {!! Html::script("assets/js/toastr/toastr.min.js") !!}
+
+ @yield('script')
+
+ <!-- JavaScripts initializations and stuff -->
+ {!! Html::script("assets/js/xenon-custom.js") !!}
+
+ </body>
+ </html>
